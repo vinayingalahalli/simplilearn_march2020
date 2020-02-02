519efbf35ea07b1a28fa1081319fcc63f2389dda
vinay <vinay.ingalahalli1989@gmail.com>
2020-02-02 23:20:45 +0530

    Feb 2nd session

---
+++ simplilearn_Feb2020/myfirstjavaproject/src/mypack/HelloWorld.java
@@ -0,0 +1,18 @@
+package mypack;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+//	class M{}
+//	public class Z{}
+//	private class D{}
+//	protected class Q{}
+//	public static class W{}
+
+}
+// class A{}
+// 
+// class B{}
+// class Z{}
