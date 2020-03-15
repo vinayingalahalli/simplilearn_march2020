4ae50fffbdb6840e9794a2d9abe39acaabba5dad
vinay <vinay.ingalahalli1989@gmail.com>
2020-03-15 23:10:47 +0530

    notes java

---
+++ simplilearn_march2020/my_first_java_project/src/mypack/MyClass.java
@@ -0,0 +1,33 @@
+package mypack;
+
+ public class MyClass {
+
+	public static void main(String vinay[]) {
+		System.out.println("Helllo");
+		System.out.println(vinay[0]);
+		System.out.println(vinay[1]);
+		System.out.println(vinay[2]);
+	}
+	public void hello() {
+		System.out.println("I am non static method");
+	}
+	
+//	private class A{
+//		
+//	}
+//	protected class B{
+//		
+//	}
+
+}
+ 
+// class D{
+//	 
+// }
+// class P{
+//	 
+// }
+// class A{
+//	 
+// }
+// 
